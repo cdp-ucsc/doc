09b381ed41305ca028df4eee1c124476d1c2b64c
Todd Graham <tgraham@ucsc.edu>
2023-04-28 17:35:23 -0700

    changed to title

--- cdp-docs/docusaurus.config.js
+++ cdp-docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Common Data Platform - Demo',
+  title: 'Common Data Platform',
   tagline: 'Project Documentation Center',
   favicon: 'img/favicon.ico',
 
