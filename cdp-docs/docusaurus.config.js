34f1db76cc387ff3f6f3569775ec8c9f31016cc6
ikpak <ikpak@ucsc.edu>
2023-05-22 16:47:21 -0700

    add languages for code block syntax

--- cdp-docs/docusaurus.config.js
+++ cdp-docs/docusaurus.config.js
@@ -155,6 +155,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['sql', 'powershell', 'bash']
       },
     }),
 };
